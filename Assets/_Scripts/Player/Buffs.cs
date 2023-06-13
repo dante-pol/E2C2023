e8ce0e9aeb13d4288818b84bf0e176800e20684e
Egor <egorglazkov71@gmail.com>
2023-06-13 16:27:29 +0300

    Add buff umbrella

---
+++ Assets/_Scripts/Player/Buffs.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEngine;
+
+public class Buffs : MonoBehaviour
+{
+    [Header("Umbrella Stats")]
+    [SerializeField] private float _timeUmbrella;
+    [HideInInspector]public bool UmbrellaPickUp;
+    [Range(-5, 0)] public float _slowingDownTheFall;
+
+
+  
+    #region Ubrella buff
+    public void Umbrella()
+    {
+        StartCoroutine(UmbrellaCorotinue());
+    }
+    public IEnumerator UmbrellaCorotinue()
+    {
+        UmbrellaPickUp = true;
+
+        yield return new WaitForSeconds(_timeUmbrella);
+
+        UmbrellaPickUp = false;
+    }
+    #endregion
+}
--- Assets/_Scripts/Player/PlayerMove.cs
+++ Assets/_Scripts/Player/PlayerMove.cs
@@ -6,29 +6,47 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] [Range(1, 50)] public float Speed;
     [SerializeField] [Range(5, 50)] public float MaxSpeed;
     [SerializeField] private VariableJoystick _joystick;
+    private Rigidbody2D _rb;
+    private Buffs _buffs;
     private float _velocityX;
-    public Rigidbody2D _rb;
-
-    [Header("Buffs")]
-    BuffsAndDebuffs _buffs;
-    [SerializeField] private float _slowingDownTheFall;
-
+    [SerializeField] private float _velocityY;
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _buffs = GetComponent<Buffs>();
     }
 
+    
     private void Update()
     {
         _velocityX = _joystick.Horizontal * Speed;
         _rb.velocity += new Vector2(_velocityX, 0);
 
-        
+        SlowingFlow();
 
-        if (Mathf.Abs(_rb.velocity.sqrMagnitude) > MaxSpeed * MaxSpeed)
+      //  if (Mathf.Abs(_rb.velocity.sqrMagnitude) > MaxSpeed * MaxSpeed)
+      //  {
+            _rb.velocity = new Vector2(MaxSpeed * _joystick.Horizontal, _velocityY);
+      //  }
+    }
+
+    public void SlowingFlow()
+    {
+        if (_buffs.UmbrellaPickUp == true)
         {
-            _rb.velocity = new Vector2(MaxSpeed * _joystick.Horizontal, _rb.velocity.y);
+            if (_rb.velocity.y < 0)
+            {
+                _velocityY = _buffs._slowingDownTheFall;
+            }
 
+            else
+            {
+                _velocityY = _rb.velocity.y;
+            }
+        }
+        else
+        {
+            _velocityY = _rb.velocity.y;
         }
     }
 }
