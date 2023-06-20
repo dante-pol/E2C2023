3ca1b5300cc7da40a3bd06ac4978434f5c73b508
Egor <egorglazkov71@gmail.com>
2023-06-20 11:16:23 +0300

    Adding debuff to rain(2).

--- Assets/_Scripts/Environment/Rain/RainDamage.cs
+++ Assets/_Scripts/Environment/Rain/RainDamage.cs
@@ -3,33 +3,40 @@ using UnityEngine;
 
 public class RainDamage : MonoBehaviour
 {
+    private Buffs _playerBuffs;
     private PlayerModel _playerModel;
     private bool isTrigger;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && collision.gameObject.GetComponent<Buffs>().UmbrellaPickUp == false)
         {
+            _playerBuffs = collision.gameObject.GetComponent<Buffs>();
             _playerModel = collision.gameObject.GetComponent<PlayerModel>();
             isTrigger = true;
             StartCoroutine(Damage());
         }
-
+        else
+        {
+            return;
+        }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             isTrigger = false;
-        }
+            collision.gameObject.GetComponent<Buffs>().Slowingdown = false;
+        } 
     }
 
     private IEnumerator Damage()
     {
         if (isTrigger == true)
         {
-            if (_playerModel.gameObject.GetComponent<Buffs>().UmbrellaPickUp == false)
+            if (_playerBuffs.UmbrellaPickUp == false)
             {
                 _playerModel.RemoveHealth(1);
+                _playerBuffs.Slowingdown = true;
                 yield return new WaitForSeconds(3);
             }
             StartCoroutine(Damage());
